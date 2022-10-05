92468284caee735b3499d03219fcfe97386bf3fe
Nabil <nabil@Adils-MacBook-Pro.local>
2022-10-05 18:37:11 -0400

    header and empty components made

--- stories/index.js
+++ stories/index.js
@@ -1,9 +1,15 @@
 import React from "react";
+
+
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+
+
 import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -140,4 +146,6 @@ storiesOf("InterviewerList", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)  
-  .add("Appointment with Time", () => <Appointment time="12pm" />);
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />) 
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
