c9d5361c6b9e5d62e8bce6d2263a6d840202fd4b
Nabil <nabil@Adils-MacBook-Pro.local>
2022-10-05 18:16:35 -0400

    added new appointment component

--- stories/index.js
+++ stories/index.js
@@ -3,6 +3,7 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index.js";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -133,3 +134,10 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+  storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)  
+  .add("Appointment with Time", () => <Appointment time="12pm" />);
