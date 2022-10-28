815edb3f1a32a486dd5bdaab91e3fbea505a35d2
Nabil Ahmed <nabilahmed98@hotmail.com>
2022-10-28 01:03:06 +0000

    to show form view when running in error mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,7 +7,7 @@ import Form from "./Form";
 import Status from "./Status";
 import Confirm from "./Confirm";
 import Error from "./Error";
-import useVisualMode from "hooks/useVisualMode";
+import useVisualMode from "../../hooks/useVisualMode";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
@@ -19,36 +19,36 @@ export default function Appointment(props) {
   const EDIT = "EDIT";
   const ERROR_SAVE = "ERROR_SAVE";
   const ERROR_DELETE = "ERROR_DELETE";
-
+  
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
-  const save = function (name, interviewer) {
+  // Call the bookInterview function in the Application component and wait for it to complete before displaying a new mode
+  const save = (name, interviewer) => {
     const interview = {
       student: name,
-      interviewer,
+      interviewer
     };
-    transition(SAVING);
-    props
-      .bookInterview(props.id, interview)
-      .then(() => {
-        transition(SHOW);
-      })
-      .catch((err) => {
-        transition(ERROR_SAVE, true);
-      });
-  };
+    transition(SAVING)
 
-  const cancel = function () {
+    props.bookInterview(props.id, interview)
+      .then(() => transition(SHOW))
+      .catch(err => {
+    console.log("hits");
+        transition(ERROR_SAVE, true)
+        console.log(err)
+      });
+  }
+
+  // Call the cancelInterview function in the Application component and wait for it to complete before displaying a new mode
+  const deleteAppointment = () => {
     transition(DELETING, true);
-    props
-      .cancelInterview(props.id)
-      .then(() => {
-        transition(EMPTY);
-      })
-      .catch((err) => {
-        transition(ERROR_DELETE, true);
+    Promise.resolve(props.cancelInterview(props.id))
+      .then(() => transition(EMPTY))
+      .catch(err => {
+        transition(ERROR_DELETE, true)
+        console.log(err)
       });
   };
 
@@ -58,19 +58,58 @@ export default function Appointment(props) {
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
+          id={props.id}
           student={props.interview.student}
           interviewer={props.interview.interviewer}
           onDelete={() => transition(CONFIRM)}
           onEdit={() => transition(EDIT)}
         />
       )}
-      {mode === CREATE && <Form interviewers={[...props.interviewers]} onCancel={back} onSave={save} />}
-      {mode === SAVING && <Status message="Saving..." />}
-      {mode === DELETING && <Status message="Deleting..." />}
-      {mode === CONFIRM && <Confirm message="Are you sure you want to delete this appointment?" onCancel={back} onConfirm={cancel} />}
-      {mode === EDIT && <Form student={props.interview.student} interviewer={props.interview.interviewer.id} interviewers={props.interviewers} onCancel={back} onSave={save} />}
-      {mode === ERROR_DELETE && <Error message='Could not cancel appointment.' onClose={back} />}
-      {mode === ERROR_SAVE && <Error message='Could not save appointment.' onClose={back} />}
+      {mode === CREATE && (
+        <Form 
+          interviewers={props.interviewers}
+          onCancel = {back}
+          onSave = {save}
+        />
+      )}
+      {mode === SAVING && (
+        <Status 
+          message="Saving"
+        />
+      )}
+      {mode === CONFIRM && (
+        <Confirm 
+          message="Are you sure you want to delete?"
+          onConfirm={deleteAppointment}
+          onCancel = {back}
+        />
+      )}
+      {mode === DELETING && (
+        <Status 
+          message="Deleting"
+        />
+      )}
+      {mode === EDIT && (
+        <Form 
+          name={props.interview.student}
+          interviewer={props.interview.interviewer.id}
+          interviewers={props.interviewers}
+          onCancel = {back}
+          onSave = {save}
+        />
+      )}
+      {mode === ERROR_SAVE && (
+        <Error 
+          message="Unable to save"
+          onClose={back}
+        />
+      )}
+      {mode === ERROR_DELETE && (
+        <Error 
+          message="Unable to delete"
+          onClose={back}
+        />
+      )}
     </article>
   );
 }
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,20 +1,10 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
   const transition = (newMode, replace = false) => {
-    if (replace) {
-      const historyCopy = [...history];
-      historyCopy.pop();
-      historyCopy.push(newMode);
-      setHistory(historyCopy);
-      setMode(newMode);
-    } else {
-      setHistory((prev) => [...prev, newMode]);
-      setMode(newMode);
-    }
+    setHistory (prev => replace ? [...prev.slice(0,prev.length-1), newMode]:[...prev, newMode])
   };
 
   const back = () => {
@@ -22,9 +12,8 @@ export default function useVisualMode(initial) {
       const historyCopy = [...history];
       historyCopy.pop();
       setHistory(historyCopy);
-      setMode(historyCopy[historyCopy.length - 1]);
     }
   };
 
-  return { mode, transition, back };
+  return { mode:history[history.length-1], transition, back };
 }
